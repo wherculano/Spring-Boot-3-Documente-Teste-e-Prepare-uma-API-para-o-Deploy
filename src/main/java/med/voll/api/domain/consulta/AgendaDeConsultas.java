f0a16e37623b0be44810677089e4d3572e1ea15c
Wagner Herculano <wagherculano@hotmail.com>
2023-04-20 16:51:56 -0300

    Configurada Entidade para agendamento de consultas

---
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -0,0 +1,46 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.infra.exception.ValidacaoException;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsultas {
+    @Autowired
+    private ConsultaRepository consultaRepository;
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public void agendar(DadosAgendamentoConsulta dados) {
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Paciente inexistente");
+        }
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Medico inexistente");
+        }
+
+        var medico = escolherMedico(dados);
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+
+        var consulta = new Consulta(null, medico, paciente, dados.data());
+        consultaRepository.save(consulta);
+
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if(dados.idMedico() != null){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+        if(dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade é obrigatória quando médico não for informado");
+        }
+
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+    }
+}
