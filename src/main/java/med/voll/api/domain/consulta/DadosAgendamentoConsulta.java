ef0dca3de0b597eb4c500203ef5f2bf39caef4b4
Wagner Herculano <wagherculano@hotmail.com>
2023-04-20 16:56:32 -0300

    Adicionada Especialidade como novo campo.

--- src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -2,6 +2,7 @@ package med.voll.api.domain.consulta;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidade;
 
 import java.time.LocalDateTime;
 
@@ -12,7 +13,9 @@ public record DadosAgendamentoConsulta(
 
         @NotNull
         @Future
-        LocalDateTime data) {
+        LocalDateTime data,
+
+        Especialidade especialidade) {
 }
 // @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 // é usado como anotação em LocalDateTime para obter a data no formato passado para o pattern
