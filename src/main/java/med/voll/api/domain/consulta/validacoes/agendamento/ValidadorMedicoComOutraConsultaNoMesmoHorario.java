1e874717dfe09e67a09f2a1ef260546287aa26e0
Wagner Herculano <wagherculano@hotmail.com>
2023-04-24 16:31:23 -0300

    Alterado nome da funcao

--- src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/agendamento/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -1,4 +1,4 @@
-package med.voll.api.domain.consulta.validacoes;
+package med.voll.api.domain.consulta.validacoes.agendamento;
 
 import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
@@ -12,7 +12,7 @@ public class ValidadorMedicoComOutraConsultaNoMesmoHorario implements ValidadorA
     private ConsultaRepository repository;
 
     public void validar(DadosAgendamentoConsulta dados) {
-        var medicoPossuiOutraConsultaNoMesmoHorario = repository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
+        var medicoPossuiOutraConsultaNoMesmoHorario = repository.existsByMedicoIdAndDataAndMotivoCancelamentoIsNull(dados.idMedico(), dados.data());
         if (medicoPossuiOutraConsultaNoMesmoHorario) {
             throw new ValidacaoException("Medico já possui outra consulta neste mesmo horario!");
         }
