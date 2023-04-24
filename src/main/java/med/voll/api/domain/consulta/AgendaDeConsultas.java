49b1e8d038bd0ada16bd9b00c822893b572a2266
Wagner Herculano <wagherculano@hotmail.com>
2023-04-24 10:18:16 -0300

    Alterado construtuor devido adicao de novo campo (motivo_cancelamento)

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -28,7 +28,7 @@ public class AgendaDeConsultas {
         var medico = escolherMedico(dados);
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
 
-        var consulta = new Consulta(null, medico, paciente, dados.data());
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
 
     }
@@ -42,5 +42,13 @@ public class AgendaDeConsultas {
         }
 
         return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if(!consultaRepository.existsById(dados.idConsulta())){
+            throw new ValidacaoException("ID da consulta informado não existe!");
+        }
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
     }
 }
